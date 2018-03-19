db2cbd6f836f378756533049901a047ead51e57f
harryxiaxia <harryxiaxia@gmail.com>
2018-03-18 22:40:48 -0500

    Added ResizingArrayStack class structure

---
+++ CodingFun/Algorithms4th/Fundamentals/ResizingArrayStack.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms4th.CustomException;
+
+namespace Algorithms4th.Fundamentals
+{
+    //Todo: change to use array
+    public class ResizingArrayStack<TItem> : IEnumerable<TItem>
+    {
+        private Node<TItem> _top;
+        private int _size;
+
+        public ResizingArrayStack()
+        {
+            _top = null;
+            _size = 0;
+        }
+
+        public void Push(TItem item)
+        {
+            var newNode = new Node<TItem> { Value = item };
+
+            if (_top == null)
+                _top = newNode;
+            else
+            {
+                newNode.Next = _top;
+                _top = newNode;
+            }
+
+            _size++;
+        }
+
+        public TItem Pop()
+        {
+            if (IsEmpty())
+                throw new NoSuchElementException("Stack is empty.");
+
+            Node<TItem> top = _top;
+            var item = _top.Value;
+            _top = _top.Next;
+            top = null;
+            _size--;
+            return item;
+        }
+
+        public TItem Peek()
+        {
+            if (IsEmpty())
+                throw new NoSuchElementException("Stack is empty.");
+
+            return _top.Value;
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
+
+        public bool IsEmpty()
+        {
+            return _size == 0;
+        }
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            return new ResizingArrayStackEnumerator<TItem>(_top);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        class ResizingArrayStackEnumerator<T> : IEnumerator<T>
+        {
+            private Node<T> _first;
+            private Node<T> _current;
+            public T Current => _current.Value;
+
+            object IEnumerator.Current => _current.Value;
+
+            public ResizingArrayStackEnumerator(Node<T> top)
+            {
+                _first = new Node<T>();
+                _first.Next = top;
+                _current = _first;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (_current.Next == null)
+                    return false;
+
+                _current = _current.Next;
+                return true;
+            }
+
+            public void Reset()
+            {
+                _current = _first;
+            }
+        }
+
+        class Node<TNode>
+        {
+            public TNode Value { get; set; }
+            public Node<TNode> Next { get; set; }
+        }
+    }
+}
